bc1be56febf183e368315a32776f5137a44150d3
Malar-Graduate-hub <ggsmalarmannan@gmail.com>
2026-05-03 11:46:28 +0530

    fix models path

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas as pd
 import os
 
 # Automatically find the correct path
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODELS_DIR = os.path.join(BASE_DIR, 'models')
 
 # Load all saved files
